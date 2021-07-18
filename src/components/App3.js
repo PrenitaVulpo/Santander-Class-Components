1a1b3d4bba54722b7910e34a4bc28fdc0df81172
PrenitaVulpo <guilhermepdl97@protonmail.com>
2021-07-18 20:41:01 -0300

    tested this

--- src/components/App3.js
+++ src/components/App3.js
@@ -4,11 +4,16 @@ class App3 extends React.Component {
   constructor(props) {
     super(props);
     this.state = { nome: '' }
+    //indica que o THIS utilizado na função é o THIS do escopo da classe
+    this.changeNome = this.changeNome.bind(this);
+  }
+  changeNome = function (evt) {
+    this.setState({ nome: evt.target.value });
   }
   render() {
     return (
       <>
-        nome: <input type='text' value={this.state.nome} />
+        nome: <input type='text' value={this.state.nome} onChange={this.changeNome} />
         <p>Olá {this.state.nome}</p>
       </>
     );
