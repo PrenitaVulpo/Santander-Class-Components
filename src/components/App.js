81045c8902b9ac69cc8755c85b8d07dee01d59d2
PrenitaVulpo <guilhermepdl97@protonmail.com>
2021-07-18 20:28:18 -0300

    add App2

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import logo from './logo.svg';
 import './App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
   render() {
     return (
       <>
